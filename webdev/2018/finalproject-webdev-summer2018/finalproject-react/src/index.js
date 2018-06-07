7c3b5458f6c2a8fb6f34fc3491752425875b4925
sijia zhu <sijiazhu@sijias-iMac.local>
2018-06-07 15:49:06 -0700

    first commit

---
+++ webdev/2018/finalproject-webdev-summer2018/finalproject-react/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Wiki from './wiki';
+ReactDOM.render(
+
+         <Wiki/>,
+
+
+
+
+
+    document.getElementById('root')
+);
+
+
+
---
+++ webdev/2018/finalproject-webdev-summer2018/finalproject-react/src/wiki.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+class Wiki extends Component{
+    render(){
+        return (
+           <h1>hello</h1>
+        )
+
+
+}
+}
+export default Wiki;
